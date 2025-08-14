c32fe8daeccad665735d0cbd82b127f8d5f518fd
Konstantin Tcepliaev <f355@f355.org>
2025-08-14 20:31:55 +0200

    e-stop is an input, not an output o\

--- include/machine_definitions.h
+++ include/machine_definitions.h
@@ -19,14 +19,15 @@ typedef struct {
   bool invert;
 } inputPin_t;
 
-#define INPUT_PINS 13
+#define INPUT_PINS 14
 #define INPUT_PIN_DESC                                                                                             \
   {                                                                                                                \
-    {"stall-alarm-spindle", 0, 19, false, false}, {"stall-alarm-x", 0, 1, false, false},                           \
-        {"stall-alarm-y", 0, 0, false, false}, {"stall-alarm-z", 3, 25, false, false},                             \
-        {"lid-sensor", 1, 8, false, true}, {"main-button", 2, 13, false, true}, {"ext-port", 0, 21, true, false},  \
-        {"endstop-x", 0, 24, false, false}, {"endstop-y", 0, 25, false, false}, {"endstop-z", 1, 1, false, false}, \
-        {"endstop-a", 1, 9, false, false}, {"probe", 2, 6, true, false}, {                                         \
+    {"e-stop", 0, 20, false, false}, {"stall-alarm-spindle", 0, 19, false, false},                                 \
+        {"stall-alarm-x", 0, 1, false, false}, {"stall-alarm-y", 0, 0, false, false},                              \
+        {"stall-alarm-z", 3, 25, false, false}, {"lid-sensor", 1, 8, false, true},                                 \
+        {"main-button", 2, 13, false, true}, {"ext-port", 0, 21, true, false}, {"endstop-x", 0, 24, false, false}, \
+        {"endstop-y", 0, 25, false, false}, {"endstop-z", 1, 1, false, false}, {"endstop-a", 1, 9, false, false},  \
+        {"probe", 2, 6, true, false}, {                                                                            \
       "tool-length-sensor", 0, 5, false, false                                                                     \
     }                                                                                                              \
   }
@@ -38,13 +39,13 @@ typedef struct {
   bool invert;
 } outputPin_t;
 
-#define OUTPUT_PINS 7
-#define OUTPUT_PIN_DESC                                                                                             \
-  {                                                                                                                 \
-    {"e-stop", 0, 20, false}, {"work-light", 2, 0, false}, {"probe-power", 0, 11, false}, {"beeper", 1, 14, false}, \
-        {"power-12v", 0, 22, false}, {"power-24v", 0, 10, false}, {                                                 \
-      "axis-enable-a", 1, 30, true                                                                                  \
-    }                                                                                                               \
+#define OUTPUT_PINS 6
+#define OUTPUT_PIN_DESC                                                                                                \
+  {                                                                                                                    \
+    {"work-light", 2, 0, false}, {"probe-power", 0, 11, false}, {"beeper", 1, 14, false}, {"power-12v", 0, 22, false}, \
+        {"power-24v", 0, 10, false}, {                                                                                 \
+      "axis-enable-a", 1, 30, true                                                                                     \
+    }                                                                                                                  \
   }
 
 #define INPUT_VARS 3
